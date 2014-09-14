f64584784034e5108770f075fb475c8eb408b86a
TBaudon <baudon.thomas@gmail.com>
2014-09-14 17:06:58 +0200

    Entity.hx

---
+++ Source/fr/radstar/Entity.hx
@@ -0,0 +1,25 @@
+package fr.radstar;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author TBaudon
+ */
+class Entity extends Sprite
+{
+	
+	public var name : String;
+	public var hitPoints : UInt;
+	public var maxHitPoint : UInt;
+	
+	public var body : 
+
+	public function new() 
+	{
+		super();
+		
+		
+	}
+	
+}
